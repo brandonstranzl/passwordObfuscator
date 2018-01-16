280bb85cb911bbbaa21ff44ce4e29343e70d6854
Brandon Stranzl <brandonstranzl@gmail.com>
2018-01-16 04:24:07 +0000

    here is the password obfuscator

---
+++ password.js
@@ -0,0 +1,11 @@
+var password = process.argv[2].toString();
+password = password.replace("a", "4");
+password = password.replace("e", "3");
+password = password.replace("o", "0");
+password = password.replace("l", "1");
+
+
+console.log(password);
+
+
+
